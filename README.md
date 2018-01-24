2b005ffb9dbb5d9ed2d8b2ae0e34c8b846be7e30
liaozhenting <15399901665@163.com>
2018-01-24 22:50:38 +0800

    更好的实现

--- README.md
+++ README.md
@@ -7,20 +7,33 @@ path string to object tree
 ```js
 import pathToTree from './pathToTress'
 
-var arr = [
-    "0000000001/",
-    "0000000001/b.png",
-    "0000000001/new_b.png",
-    "0000000001/watermark.zip",
-    "0000000001/ass",
-    "0000000001/ass/ass.txt",
-    "0000000001/tes",
-    "0000000001/tes/test.txt",
+ var arr=[
+    "root/",
+    "root/a/",
+    "root/a/new_b.png",
+    "root/a/qa/",
+    "root/a/qa/新建文本文档 (3).txt",
+    "root/asdfasdfasdfasdfasdfasdfasdf.txt",
+    "root/b.png",
+    "root/instqj_gfzqhk.exe",
+    "root/jupyter_notebook.png",
+    "root/new_b.png",
+    "root/output/new_b.png",
+    "root/soffice.exe",
+    "root/ti/asdfasdfasdfasdfasdfasdfasdf.txt",
+    "root/watermark.zip",
+    "root/中华人民共和国国民经济和社会发展 第十三个五年规划纲要 .pdf",
+    "root/国务院发布《中国制造2025》%28全文%29.pdf",
+    "root/新建文本文档 (3).txt",
+    "root/新建文本文档.txt",
+    "root/沧海一声笑.docx",
+    "root/理光C2011SP.exe"
 ]
 
 console.log(pathToTree(arr))
-
+console.log(traverseTree(pathToTree(arr)))
 ```
 output
 ![image](https://raw.githubusercontent.com/Liaozhenting/pathToTree/master/example.png)
+![image](https://raw.githubusercontent.com/Liaozhenting/pathToTree/master/example2.png)
 
--- pathToTree.js
+++ pathToTree.js
@@ -1,148 +1,50 @@
-const compare = (() => {
-    function compareArray(a, b) {
-        if (a.length !== b.length) {
-            return false;
-        }
-        const length = a.length;
-        for (let i = 0; i < length; i++) {
-            if (!compare(a[i], b[i])) {
-                return false;
+const pathToTree = (input) => {
+    var output = [];
+    for (var i = 0; i < input.length; i++) {
+        var chain       = input[i].split("/");
+        var currentNode = output;
+        for (var j = 0; j < chain.length; j++) {
+            if (chain[j] === '') {
+                break;
             }
-        }
+            var wantedNode = chain[j];
 
-        return true;
-    }
+            var lastNode = currentNode;
 
-    function compareObject(a, b) {
-        const keya = Object.keys(a);
-        const keyb = Object.keys(b);
-
-        if (keya.length !== keyb.length) {
-            return false;
-        }
-
-        return keya.every(key => {
-            if (!compare(a[key], b[key])) {
-                return false;
-            }
-            return true;
-        });
-    }
-
-    function compare(a, b) {
-        if (a === b) {
-            return true;
-        }
-
-        if (typeof a !== typeof b || a === null || b === null) {
-            return false;
-        }
-
-        if (Array.isArray(a)) {
-            if (!Array.isArray(b)) {
-                return false;
-            }
-            return compareArray(a, b);
-        }
-
-        if (typeof a === "object") {
-            return compareObject(a, b);
-        }
-
-        return false;
-    }
-
-    return compare;
-})();
-
-
-function arrayToPid(data) {
-
-    let sorted = data.sort((a, b) => {
-        return a.split('/').length - b.split('/').length
-    })
-    console.log(sorted)
-    let index = 1;
-
-    let new_arr = [];
-
-    sorted.map((ele, i) => {
-        let obj           = {}
-            obj.id        = index;
-            obj.title     = ele
-        let completedPath = ele.split('/')
-            obj.pid       = 0;
-        new_arr.map((new_arr_ele, new_arr_index) => {
-
-            let new_arr_ele_completedPath = new_arr_ele.title.split('/');
-            let front_string              = completedPath.slice(0, completedPath.length - 1);
-            if (compare(new_arr_ele_completedPath.slice(-1), [''])) {
-                if (compare(front_string, new_arr_ele_completedPath.slice(0, new_arr_ele_completedPath.length - 1))) {
-                    obj.pid = new_arr_ele.id;
-                    return;
+            for (var k = 0; k < currentNode.length; k++) {
+                if (currentNode[k].title == wantedNode) {
+                    currentNode = currentNode[k].children;
+                    break;
                 }
             }
-            if (compare(front_string, new_arr_ele_completedPath)) {
-                obj.pid = new_arr_ele.id;
-                return
-            }
-
-        })
-        index++
-
-        new_arr.push(obj);
-
-    })
-    return new_arr
-}
-
-function arrayToJson(treeArray) {
-    var r      = [];
-    var tmpMap = {};
-
-    for (var i = 0, l = treeArray.length; i < l; i++) {
-        // 以每条数据的id作为obj的key值，数据作为value值存入到一个临时对象里面
-        tmpMap[treeArray[i]["id"]] = treeArray[i];
-    }
-
-    for (i = 0, l = treeArray.length; i < l; i++) {
-        var key = tmpMap[treeArray[i]["pid"]];
-
-        //循环每一条数据的pid，假如这个临时对象有这个key值，就代表这个key对应的数据有children，需要Push进去
-        if (key) {
-            if (!key["children"]) {
-                key["children"] = [];
-                key["children"].push(treeArray[i]);
+            // If we couldn't find an item in this list of children
+            // that has the right name, create one:
+            if (lastNode == currentNode) {
+                var newNode     = currentNode[k] = { key: input[i], title: wantedNode, children: [] };
+                    currentNode = newNode.children;
             } else {
-                key["children"].push(treeArray[i]);
+                delete currentNode.children
             }
-        } else {
-            //如果没有这个Key值，那就代表没有父级,直接放在最外层
-            r.push(treeArray[i]);
         }
     }
-    return r
+    return output;
+}
+// traverse tree to do something
+const traverseTree = function(data) {
+    return data.map((item) => {
+
+        item.title = item.title.length > 10 ? item.title.substring(0,7)+"..." : item.title
+        if (item.children.length === 0) {
+            delete item.children
+            return item
+        } else {
+            beautify(item.children)
+            return item
+        }
+    })
 }
 
-function main(arr) {
-    return arrayToJson(arrayToPid(arr))
+export default {
+    pathToTree:pathToTree,
+    traverseTree:traverseTree
 }
-
-export default main;
-
-
-
-/*****使用
-var arr = [
-    "0000000001/",
-    "0000000001/b.png",
-    "0000000001/new_b.png",
-    "0000000001/watermark.zip",
-    "0000000001/ass",
-    "0000000001/ass/ass.txt",
-    "0000000001/tes",
-    "0000000001/tes/test.txt",
-]
-
-console.log(main(arr))
-******/
