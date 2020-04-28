5168b9bd8eccf8adb87a6a2667893585628bdbb9
廖振廷 <1782708098@qq.com>
2020-04-28 11:27:12 +0800

    refactor: 命名

--- pathToTree.js
+++ pathToTree.js
@@ -1,44 +1,39 @@
-const pathToTree = (input) => {
-    var output = [];
-    for (var i = 0; i < input.length; i++) {
-        var chain       = input[i].split("/");
-        var currentNode = output;
-        for (var j = 0; j < chain.length; j++) {
-            if (chain[j] === '') {
-                break;
-            }
-            var wantedNode = chain[j];
+pathToTree =  (input) => {
+  var root = [];
+  for (let i=0;i<input.length;i++){
+    var chain = input[i].split("/");
+    var currentHierarchy = root;
+    for(var j = 0; j < chain.length;j++){
+      var wantedNode = chain[j]
+      // 最后一级是文件夹
+      if(wantedNode === ''){
+        break;
+      }
+      let lastHierarchy = currentHierarchy;
 
-            var lastNode = currentNode;
-
-            for (var k = 0; k < currentNode.length; k++) {
-                if (currentNode[k].title == wantedNode) {
-                    currentNode = currentNode[k].children;
-                    break;
-                }
-            }
-            // If we couldn't find an item in this list of children
-            // that has the right name, create one:
-            if (lastNode == currentNode) {
-                //创造对于层级的key
-                var node =input[i];
-                var newNode = currentNode[k] = {
-                    key:chain.slice(0,j+1).join("/"),
-                    title:wantedNode,
-                    children:[]
-                }
-                currentNode = newNode.children;
-                //原来的key保留
-                // var newNode     = currentNode[k] = { key: input[i], title: wantedNode, children: [] };
-                //     currentNode = newNode.children;
-            } else {
-                delete currentNode.children
-            }
+      // 遍历root是否已有该层级
+      for(let k = 0; k < currentHierarchy.length;k++){
+        if(currentHierarchy[k].title === wantedNode){
+          currentHierarchy = currentHierarchy[k].children;
+          break;
         }
+      }
+
+      if(lastHierarchy === currentHierarchy) {
+        let newNode = {
+          key: input[i],
+          title: wantedNode,
+          children: []
+        };
+        // 文件，最后一个字符不是"/“符号
+        if(j=== chain.length-1){
+          delete newNode.children;
+        }
+        currentHierarchy.push(newNode);
+        currentHierarchy = newNode.children;
+      }
     }
-    return output;
+  }
+
+  return root;
 }
-
-
-
-export default pathToTree;
